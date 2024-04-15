b832f3b67a41cb816025322698ca0aa4d1f8ec67
Otávio Almeida <otavioalmeida650@gmail.com>
2024-04-15 14:46:23 -0300

    submodules

--- client/src/components/SpinnerLoader.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { Spinner } from 'flowbite-svelte';
-	export let size: number = 16;
-</script>
-
-<div class="flex min-h-full min-w-full items-center justify-center">
-	<Spinner {size} />
-</div>
--- client/src/lib/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SERVER_HOST = "https://url-shortner-6pxn.onrender.com";
-// export const SERVER_HOST = "http://localhost:8080";
